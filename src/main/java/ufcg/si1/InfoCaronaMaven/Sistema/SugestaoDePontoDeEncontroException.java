5bdbc4437a8c68000a737ffa6be00742a4499601
Felipeefb <malkavianbr@gmail.com>
2012-05-21 16:53:56 -0300

    Refatorando as Excecoes, agora utilizem as Excecoes que estão no pacote Sistema, UsuarioException, CaronaException, IDException, SolicitacaoDeVagaException, SugestaoDePontoDeEncontroException, lembrando que na hora de lançar a exceção é nescessario passar uma string com o erro a ser lançado

---
+++ src/main/java/ufcg/si1/InfoCaronaMaven/Sistema/SugestaoDePontoDeEncontroException.java
@@ -0,0 +1,9 @@
+package ufcg.si1.InfoCaronaMaven.Sistema;
+
+
+public class SugestaoDePontoDeEncontroException extends Exception {
+	
+    public SugestaoDePontoDeEncontroException(String mesage){
+    	super(mesage);
+    }
+}
